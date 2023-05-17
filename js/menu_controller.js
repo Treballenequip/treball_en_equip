df0e33dc8d8b1c1c15966f910e49b4ccab06f193
Francisco <104916137+FFernandezC@users.noreply.github.com>
2023-05-17 14:44:07 +0200

    Menu principal acabat

--- js/menu_controller.js
+++ js/menu_controller.js
@@ -1,15 +1,16 @@
+"use strict";
+
+//Button to go to Game
 function start_game(){
 	loadpage("./html/joc.html");
 }
 
+//Button to go to Options
 function options (){
 	loadpage("../index.html")
 }
 
-function load(){
-	loadpage("./html/load.html");
-}
-
+//Button to Exit
 function exit (){
 	loadpage("../index.html")
 }
