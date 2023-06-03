7f91b59894d210dc04798cb5b780999564d52a88
Joel <60098544+Joelrumex@users.noreply.github.com>
2023-06-03 17:15:31 +0200

    Crear i implementar periodistes

--- js/platformgame.js
+++ js/platformgame.js
@@ -5,6 +5,7 @@ class PlatformScene extends Phaser.Scene {
         super('PlatformScene');
 		this.platforms = null;
 		this.player = null;
+        this.enemy = null;
 		this.cursors = null;
 		this.stars = null;
 		this.score = 0;
@@ -12,32 +13,34 @@ class PlatformScene extends Phaser.Scene {
 		this.bombs = null;
 		this.gameOver = false;
         this.moveCam = false;
+        this.distanciaEnemy = 600;
     }
     preload (){	
 		this.load.image('sky', '../assets/sky.jpg');
+        this.load.image('cameraman', '../assets/camera_man.png');
 		this.load.spritesheet('dude',
 			'../assets/dude.png',
 			{ frameWidth: 32, frameHeight: 48 }
 		);
 	}
     create (){	
-		this.cameras.main.setBounds(400, 0, 1920 * 2, 0);
-        this.physics.world.setBounds(0, 0, 1920 * 2, 1080 * 2);
+		this.cameras.main.setBounds(0, 0, 3840 * 2, 0);
+        this.physics.world.setBounds(0, 0, 3840 * 2, 1080 * 2);
 
         
         //  Mash 4 images together to create our background
-        for (let x = 0; x < 10; x++)
+        for (let x = 0; x < 30; x++)
         {
             this.add.image(700 * x, 0, 'sky').setOrigin(0);
             this.add.image(700 * x, 400, 'sky').setOrigin(0).setFlipY(true);
         }
-
         this.cursors = this.input.keyboard.createCursorKeys();
 
 
 		{	// Creem player i definim animacions
-			this.player = this.physics.add.sprite(400, 350, 'dude');
-            //376, 351, 326
+            
+			this.player = this.physics.add.sprite(200, 340, 'dude');
+            //415.6, 375.6, 326
 			this.player.setBounce(0.2);
 			this.player.setCollideWorldBounds(true);
 			
@@ -61,13 +64,26 @@ class PlatformScene extends Phaser.Scene {
 				repeat: -1
 			});
 		}
+        
+        this.reporters = this.physics.add.group();
+        this.createEnemy();
+        this.player.setScale(0.6);
+        
+        this.physics.add.collider(this.player, this.reporters, 
+            (body1, body2)=>this.hitBomb(body1, body2));
         this.cameras.main.startFollow(this.player, true);
-        this.cameras.main.setZoom(2);
+        this.cameras.main.setZoom(2.01);
 	}
 	update (){	
+        if (this.gameOver) return;
 		const cam = this.cameras.main;
 
-
+        if (this.player.body.position.x >= this.distanciaEnemy) {
+            this.distanciaEnemy += 500;
+            console.log(this.distanciaEnemy)
+            this.createEnemy();
+            this.createEnemy();
+        }
         if (this.moveCam)
         {
             if (this.cursors.left.isDown)
@@ -92,20 +108,45 @@ class PlatformScene extends Phaser.Scene {
         {
             this.player.anims.play('right', true);
             this.player.setVelocityX(100);
-            console.log(this.player.body.position.y)
-            if (this.cursors.left.isDown && this.player.body.position.y != 326)
+            if (this.cursors.left.isDown && this.player.body.position.y != 325.6)
             {
-                this.player.setPosition(this.player.body.position.x+10,350);
+                this.player.setPosition(this.player.body.position.x+5,340);
             }
-            else if (this.cursors.down.isDown && this.player.body.position.y != 351)
+            else if (this.cursors.down.isDown && this.player.body.position.y != 375.6)
             {
-                this.player.setPosition(this.player.body.position.x+10,375);
+                this.player.setPosition(this.player.body.position.x+5,390   );
             }
-            else if (this.cursors.right.isDown && this.player.body.position.y != 376)
+            else if (this.cursors.right.isDown && this.player.body.position.y != 415.6)
             {
-                this.player.setPosition(this.player.body.position.x+10,400);
+                this.player.setPosition(this.player.body.position.x+5,430);
             }
         }
+        console.log(this.player.body.position.y);
+	}
+    createEnemy(){
+        var rng = Phaser.Math.Between(1, 3);
+        var y = 0;
+        if (rng == 1) {
+            y = 340;
+        }
+        else if (rng == 2){
+            y = 390;
+        }
+        else if (rng == 3){
+            y = 430;
+        }
+        var enemy = this.reporters.create(this.distanciaEnemy, y, 'cameraman');
+        enemy.setCollideWorldBounds(true);
+        enemy.setScale(.04);
+    }
+    hitBomb(player, bomb){
+		if (this.gameOver) 
+			return;
+		this.physics.pause();
+		this.player.setTint(0xff0000);
+		this.player.anims.play('turn');
+		this.gameOver = true;
+		setTimeout(()=>loadpage("../"), 3000);
 	}
 }
 
